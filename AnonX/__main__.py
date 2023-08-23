b966977812f9297e74c10dc5772b4fc82681fb8e
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-23 15:33:22 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -53,7 +53,7 @@ async def init():
     await Anon.start()
     try:
         await Anon.stream_call(
-            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
+            "https://te.legra.ph/file/69aa8cd074ced3bb08e85.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AnonX").error(
