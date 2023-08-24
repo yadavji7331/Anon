c972273d175ca9a8dcef3041e63b8b4b98428d2a
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-24 20:02:33 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -95,10 +95,10 @@ async def gen_thumb(videoid, user_id):
 
         Xcenter = youtube.width / 2
         Ycenter = youtube.height / 2
-        x1 = Xcenter - 150
-        y1 = Ycenter - 230
-        x2 = Xcenter + 150
-        y2 = Ycenter + 230
+        x1 = Xcenter - 250
+        y1 = Ycenter - 240
+        x2 = Xcenter + 250
+        y2 = Ycenter + 240
         logo = youtube.crop((x1, y1, x2, y2))
         logo.thumbnail((520, 520), Image.ANTIALIAS)
         logo.save(f"cache/chop{videoid}.png")
@@ -113,7 +113,7 @@ async def gen_thumb(videoid, user_id):
         width = int((1280 - 500) / 1)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 2, 30), mask=logo)
-        background.paste(x, (200, 190), mask=x)
+        background.paste(x, (180, 170), mask=x)
         background.paste(image3, (2, 2), mask=image3)
 
         draw = ImageDraw.Draw(background)
