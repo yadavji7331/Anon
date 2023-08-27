9036b9c35a2b269a9bc4ff2e15a548322f30b2ff
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-27 23:54:53 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -112,7 +112,7 @@ async def gen_thumb(videoid, user_id):
         logo.thumbnail((800, 800), Image.ANTIALIAS)
         width = int((1280 - 800) / 1)
         background = Image.open(f"cache/temp{videoid}.png")
-        background.paste(logo, (width + 1, 50), mask=logo)
+        background.paste(logo, (width + 1, 10), mask=logo)
         background.paste(x, (97, 69), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
@@ -250,7 +250,7 @@ async def gen_qthumb(videoid, user_id):
         crop_img = Image.open(f"cache/cropped{videoid}.png")
         logo = crop_img.convert("RGBA")
         logo.thumbnail((800, 800), Image.ANTIALIAS)
-        width = int((1280 - 800) / 1.50)
+        width = int((1280 - 800) / 1.10)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 2, 20), mask=logo)
         background.paste(x, (97, 69), mask=x)
