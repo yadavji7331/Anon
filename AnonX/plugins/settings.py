665e47f1c0c122f2e627de1992fd130c2350cfa9
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-27 23:44:26 +0530

    Update settings.py

--- AnonX/plugins/settings.py
+++ AnonX/plugins/settings.py
@@ -63,7 +63,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/c8e25bb16d5c272e6d041.mp4"),
+        InputMediaVideo("https://te.legra.ph/file/f72a3fef47603323a4abd.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
