d64adfac1c81cc5e59bfc531a7aa7adaa92df134
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-30 20:51:07 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -109,11 +109,11 @@ async def gen_thumb(videoid, user_id):
 
         crop_img = Image.open(f"cache/cropped{videoid}.png")
         logo = crop_img.convert("RGBA")
-        logo.thumbnail((400, 400), Image.ANTIALIAS)
-        width = int((1280 - 400) / 11)
+        logo.thumbnail((500, 500), Image.ANTIALIAS)
+        width = int((1280 - 500) / 11)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 5, 120), mask=logo)
-        background.paste(x, (800, 200), mask=x)
+        background.paste(x, (850, 200), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
