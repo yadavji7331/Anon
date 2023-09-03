783cb160374d337c106d748701be4193d634973e
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-09-03 15:42:43 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -250,10 +250,10 @@ async def gen_qthumb(videoid, user_id):
         crop_img = Image.open(f"cache/cropped{videoid}.png")
         logo = crop_img.convert("RGBA")
         logo.thumbnail((500, 500), Image.ANTIALIAS)
-        width = int((1280 - 500) / 1.10)
+        width = int((1280 - 500) / 11)
         background = Image.open(f"cache/temp{videoid}.png")
-        background.paste(logo, (width + 2, 20), mask=logo)
-        background.paste(x, (135, 135), mask=x)
+        background.paste(logo, (width + 11, 100), mask=logo)
+        background.paste(x, (850, 170), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
