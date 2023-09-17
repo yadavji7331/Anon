4572646dee75a0954562ccc1987ddc34518adaf4
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-09-17 21:01:06 +0530

    Update webshot.py

--- AnonX/plugins/webshot.py
+++ AnonX/plugins/webshot.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Romeo import app
-from Romeo.utils.errors import capture_err
+from AnonX import app
+from AnonX.utils.errors import capture_err
 
 
 @app.on_message(filters.command("webss"))
